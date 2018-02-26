4c2312b3b6e2743903fc52340a39901c8b35a6e0
x404 <kronos2003@gmail.com>
2018-02-26 22:45:21 +0200

    Начата всплывающая форма "Задать вопрос"

--- gulpfile.js
+++ gulpfile.js
@@ -65,6 +65,7 @@ gulp.task('compress', function(){
 				config.libsDir + '/jquery-validation/dist/jquery.validate.js',
 				config.templateDir + '/js/util.js',
 				config.templateDir + '/js/tab.js',
+				config.templateDir + '/js/modal.js',
 				config.templateDir + '/js/popper.min.js'
 			]),
 			concat('libs.min.js'), // Собираем их в кучу в новом файле libs.min.js
