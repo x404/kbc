188c6dd7b129bd82c00792acfa1ca24d4d93b127
x404 <kronos2003@gmail.com>
2018-03-11 19:12:13 +0200

    Переделаны якарты на использование массивов объектов

--- app/js/map-objects.js
+++ app/js/map-objects.js
@@ -10,7 +10,9 @@ function init(){
 		var myMap = new ymaps.Map('map', {
 			center: [55.998180, 92.844331],
 			zoom: 12
-		}),
+		});
+
+		myPlacemark = new Array();
 
 // /////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // ОБРАЗОВАНИЕ
@@ -23,27 +25,32 @@ function init(){
 			hideIconOnBalloonOpen: false
 		});
 
-		// создаем метки и прописываем ее свойства 
-		educationCollection.add(new ymaps.Placemark(
-			[55.980180, 92.914331],
+		obj = [
 			{
-				hintContent: 'Головной офис (660019, г. Красноярск, ул. Мусоргского, 18)'
+				coord: [55.980180, 92.914331],
+				properties :{
+					hintContent: 'Головной офис (660019, г. Красноярск, ул. Мусоргского, 18)'
+				}
+			},
+			{
+				coord: [56.016768, 92.875012],
+				properties: {
+					hintContent: 'Офис продаж «Центр» (г. Красноярск, ул. Ады Лебедевой, 18)'
+				}
+			},
+			{
+				coord: [56.013663, 92.800407],
+				properties: {
+					hintContent: 'Офис продаж «Октябрьский» (г. Красноярск, ул. Академика Киренского, 71)'
+				}
 			}
-		));
+		];
 
-		educationCollection.add(new ymaps.Placemark(
-			[56.016768, 92.875012],
-			{
-				hintContent: 'Офис продаж «Центр» (г. Красноярск, ул. Ады Лебедевой, 18)'
-			}
-		));
-
-		educationCollection.add(new ymaps.Placemark(
-			[56.013663, 92.800407],
-			{
-				hintContent: 'Офис продаж «Октябрьский» (г. Красноярск, ул. Академика Киренского, 71)'
-			}
-		));
+		// создаем метки и прописываем им свойства 
+		for (var i = 0; i < obj.length; i++) {
+			myPlacemark[i] = new ymaps.Placemark(obj[i].coord, obj[i].properties);
+			educationCollection.add(myPlacemark[i])
+		}
 		// добавляем коллекцию в геообъект
 		myMap.geoObjects.add(educationCollection);
 
@@ -59,27 +66,32 @@ function init(){
 			hideIconOnBalloonOpen: false
 		});
 
-		// создаем метки и прописываем ее свойства 
-		shopCollection.add(new ymaps.Placemark(
-			[55.997959, 92.957564],
+		obj = [
 			{
-				hintContent: 'Головной офис (660019, г. Красноярск, ул. Мусоргского, 18)'
+				coord: [55.997959, 92.957564],
+				properties :{
+					hintContent: 'Головной офис (660019, г. Красноярск, ул. Мусоргского, 18)'
+				}
+			},
+			{
+				coord: [55.975586, 92.833046],
+				properties: {
+					hintContent: 'Офис продаж «Центр» (г. Красноярск, ул. Ады Лебедевой, 18)'
+				}
+			},
+			{
+				coord: [56.011761, 92.887977],
+				properties: {
+					hintContent: 'Офис продаж «Октябрьский» (г. Красноярск, ул. Академика Киренского, 71)'
+				}
 			}
-		));
+		];
 
-		shopCollection.add(new ymaps.Placemark(
-			[55.975586, 92.833046],
-			{
-				hintContent: 'Офис продаж «Центр» (г. Красноярск, ул. Ады Лебедевой, 18)'
-			}
-		));
-
-		shopCollection.add(new ymaps.Placemark(
-			[56.011761, 92.887977],
-			{
-				hintContent: 'Офис продаж «Октябрьский» (г. Красноярск, ул. Академика Киренского, 71)'
-			}
-		));
+		// создаем метки и прописываем им свойства 
+		for (var i = 0; i < obj.length; i++) {
+			myPlacemark[i] = new ymaps.Placemark(obj[i].coord, obj[i].properties);
+			shopCollection.add(myPlacemark[i])
+		};
 		// добавляем коллекцию в геообъект
 		myMap.geoObjects.add(shopCollection);
 // /////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -95,27 +107,33 @@ function init(){
 			hideIconOnBalloonOpen: false
 		});
 
-		// создаем метки и прописываем ее свойства 
-		sportCollection.add(new ymaps.Placemark(
-			[56.005029, 92.899993],
+		obj = [
 			{
-				hintContent: 'Головной офис (660019, г. Красноярск, ул. Мусоргского, 18)'
+				coord: [56.005029, 92.899993],
+				properties :{
+					hintContent: 'Головной офис (660019, г. Красноярск, ул. Мусоргского, 18)'
+				}
+			},
+			{
+				coord: [56.018300, 92.823776],
+				properties: {
+					hintContent: 'Офис продаж «Центр» (г. Красноярск, ул. Ады Лебедевой, 18)'
+				}
+			},
+			{
+				coord: [56.015607, 92.911666],
+				properties: {
+					hintContent: 'Офис продаж «Октябрьский» (г. Красноярск, ул. Академика Киренского, 71)'
+				}
 			}
-		));
+		];
 
-		sportCollection.add(new ymaps.Placemark(
-			[56.018300, 92.823776],
-			{
-				hintContent: 'Офис продаж «Центр» (г. Красноярск, ул. Ады Лебедевой, 18)'
-			}
-		));
+		// создаем метки и прописываем им свойства 
+		for (var i = 0; i < obj.length; i++) {
+			myPlacemark[i] = new ymaps.Placemark(obj[i].coord, obj[i].properties);
+			sportCollection.add(myPlacemark[i])
+		};
 
-		sportCollection.add(new ymaps.Placemark(
-			[56.015607, 92.911666],
-			{
-				hintContent: 'Офис продаж «Октябрьский» (г. Красноярск, ул. Академика Киренского, 71)'
-			}
-		));
 		// добавляем коллекцию в геообъект
 		myMap.geoObjects.add(sportCollection);
 // /////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -131,27 +149,33 @@ function init(){
 			hideIconOnBalloonOpen: false
 		});
 
-		// создаем метки и прописываем ее свойства 
-		medicineCollection.add(new ymaps.Placemark(
-			[55.987712, 92.885574],
+		obj = [
 			{
-				hintContent: 'Головной офис (660019, г. Красноярск, ул. Мусоргского, 18)'
+				coord: [55.987712, 92.885574],
+				properties :{
+					hintContent: 'Головной офис (660019, г. Красноярск, ул. Мусоргского, 18)'
+				}
+			},
+			{
+				coord: [55.984392, 92.860340],
+				properties: {
+					hintContent: 'Офис продаж «Центр» (г. Красноярск, ул. Ады Лебедевой, 18)'
+				}
+			},
+			{
+				coord: [56.001903, 92.814334],
+				properties: {
+					hintContent: 'Офис продаж «Октябрьский» (г. Красноярск, ул. Академика Киренского, 71)'
+				}
 			}
-		));
+		];
 
-		medicineCollection.add(new ymaps.Placemark(
-			[55.984392, 92.860340],
-			{
-				hintContent: 'Офис продаж «Центр» (г. Красноярск, ул. Ады Лебедевой, 18)'
-			}
-		));
+		// создаем метки и прописываем им свойства 
+		for (var i = 0; i < obj.length; i++) {
+			myPlacemark[i] = new ymaps.Placemark(obj[i].coord, obj[i].properties);
+			medicineCollection.add(myPlacemark[i])
+		};
 
-		medicineCollection.add(new ymaps.Placemark(
-			[56.001903, 92.814334],
-			{
-				hintContent: 'Офис продаж «Октябрьский» (г. Красноярск, ул. Академика Киренского, 71)'
-			}
-		));
 		// добавляем коллекцию в геообъект
 		myMap.geoObjects.add(medicineCollection);
 // /////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -167,27 +191,34 @@ function init(){
 			hideIconOnBalloonOpen: false
 		});
 
-		// создаем метки и прописываем ее свойства 
-		eatCollection.add(new ymaps.Placemark(
-			[56.010366, 92.943767],
+		obj = [
 			{
-				hintContent: 'Головной офис (660019, г. Красноярск, ул. Мусоргского, 18)'
+				coord: [56.010366, 92.943767],
+				properties :{
+					hintContent: 'Головной офис (660019, г. Красноярск, ул. Мусоргского, 18)'
+				}
+			},
+			{
+				coord: [55.994014, 92.900852],
+				properties: {
+					hintContent: 'Офис продаж «Центр» (г. Красноярск, ул. Ады Лебедевой, 18)'
+				}
+			},
+			{
+				coord: [55.972650, 92.939647],
+				properties: {
+					hintContent: 'Офис продаж «Октябрьский» (г. Красноярск, ул. Академика Киренского, 71)'
+				}
 			}
-		));
+		];
 
-		eatCollection.add(new ymaps.Placemark(
-			[55.994014, 92.900852],
-			{
-				hintContent: 'Офис продаж «Центр» (г. Красноярск, ул. Ады Лебедевой, 18)'
-			}
-		));
+		// создаем метки и прописываем им свойства 
+		for (var i = 0; i < obj.length; i++) {
+			myPlacemark[i] = new ymaps.Placemark(obj[i].coord, obj[i].properties);
+			eatCollection.add(myPlacemark[i])
+		};
 
-		eatCollection.add(new ymaps.Placemark(
-			[55.972650, 92.939647],
-			{
-				hintContent: 'Офис продаж «Октябрьский» (г. Красноярск, ул. Академика Киренского, 71)'
-			}
-		));
+
 		// добавляем коллекцию в геообъект
 		myMap.geoObjects.add(eatCollection);
 // /////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -204,26 +235,32 @@ function init(){
 		});
 
 		// создаем метки и прописываем ее свойства 
-		transportCollection.add(new ymaps.Placemark(
-			[56.011905, 92.813304],
+		obj = [
 			{
-				hintContent: 'Головной офис (660019, г. Красноярск, ул. Мусоргского, 18)'
+				coord: [56.011905, 92.813304],
+				properties :{
+					hintContent: 'Головной офис (660019, г. Красноярск, ул. Мусоргского, 18)'
+				}
+			},
+			{
+				coord: [55.998440, 92.878536],
+				properties: {
+					hintContent: 'Офис продаж «Центр» (г. Красноярск, ул. Ады Лебедевой, 18)'
+				}
+			},
+			{
+				coord: [55.986894, 92.812961],
+				properties: {
+					hintContent: 'Офис продаж «Октябрьский» (г. Красноярск, ул. Академика Киренского, 71)'
+				}
 			}
-		));
+		];
 
-		transportCollection.add(new ymaps.Placemark(
-			[55.998440, 92.878536],
-			{
-				hintContent: 'Офис продаж «Центр» (г. Красноярск, ул. Ады Лебедевой, 18)'
-			}
-		));
-
-		transportCollection.add(new ymaps.Placemark(
-			[55.986894, 92.812961],
-			{
-				hintContent: 'Офис продаж «Октябрьский» (г. Красноярск, ул. Академика Киренского, 71)'
-			}
-		));
+		// создаем метки и прописываем им свойства 
+		for (var i = 0; i < obj.length; i++) {
+			myPlacemark[i] = new ymaps.Placemark(obj[i].coord, obj[i].properties);
+			transportCollection.add(myPlacemark[i])
+		};
 		// добавляем коллекцию в геообъект
 		myMap.geoObjects.add(transportCollection);
 // /////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -240,32 +277,40 @@ function init(){
 
 		// создаем коллекции и параметры меток в ней
 		houseCollection = new ymaps.GeoObjectCollection({},{
-			// iconLayout: 'default#image',
-			// iconImageHref: '/images/icons/balloon-label.png',
-			// iconImageSize: [58, 60],
-			// iconImageOffset: [-29, -60],
 			hideIconOnBalloonOpen: false,
 			iconLayout: 'cluster#mystyle',
 			iconContentLayout: HouseIconLayout
 		});
 
-		houseCollection.add(new ymaps.Placemark(
-			[55.991705, 92.830814],
-			{
-				hintContent: 'Головной офис (660019, г. Красноярск, ул. Мусоргского, 18)',
-				iconContentLayout: '1.1'
-			}
-		));
 
-		houseCollection.add(new ymaps.Placemark(
-			[55.993052, 92.873386],
+
+		// создаем метки и прописываем ее свойства 
+		obj = [
 			{
-				hintContent: 'Офис продаж «Центр» (г. Красноярск, ул. Ады Лебедевой, 18)',
-				iconContentLayout: '1.6'
+				coord: [55.991705, 92.830814],
+				properties :{
+					hintContent: 'Головной офис (660019, г. Красноярск, ул. Мусоргского, 18)',
+					iconContentLayout: '1.1'
+				}
+			},
+			{
+				coord: [55.993052, 92.873386],
+				properties: {
+					hintContent: 'Офис продаж «Центр» (г. Красноярск, ул. Ады Лебедевой, 18)',
+					iconContentLayout: '1.6'
+				}
 			}
-		));
+		];
+
+		// создаем метки и прописываем им свойства 
+		for (var i = 0; i < obj.length; i++) {
+			myPlacemark[i] = new ymaps.Placemark(obj[i].coord, obj[i].properties);
+			houseCollection.add(myPlacemark[i])
+		};
+
+
 		myMap.geoObjects.add(houseCollection);
-
+// /////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
 		// блокируем зум и включаем его по щелчку на карте
