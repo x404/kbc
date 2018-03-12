a9167191995bea9700f349b90ed0a935d05784dd
x404 <kronos2003@gmail.com>
2018-03-12 16:16:21 +0200

    мелкие правки

--- app/js/app.js
+++ app/js/app.js
@@ -30,9 +30,9 @@ var app = new Vue({
 
 
 
-function declOfNum(number, titles) {  
-    cases = [2, 0, 1, 1, 1, 2];  
-    return titles[ (number%100>4 && number%100<20)? 2 : cases[(number%10<5)?number%10:5] ];  
-}
+// function declOfNum(number, titles) {  
+//     cases = [2, 0, 1, 1, 1, 2];  
+//     return titles[ (number%100>4 && number%100<20)? 2 : cases[(number%10<5)?number%10:5] ];  
+// }
 
-declOfNum(count, ['метр', 'метра', 'метров']);
+// declOfNum(count, ['метр', 'метра', 'метров']);
